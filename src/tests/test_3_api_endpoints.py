11abc80b423909ef061ed41a25bbd544210d360a
Vladimir Korshak <vladimirkorshak@gmail.com>
2018-12-07 21:17:48 +0200

    - attempt to fix travis endpoint bug

--- src/tests/test_3_api_endpoints.py
+++ src/tests/test_3_api_endpoints.py
@@ -3,9 +3,8 @@ import os
 
 import pytest
 
-pytestmark = pytest.mark.skipif(os.getenv("TRAVIS", False) is True, reason="tests for localhost only")
 
-@pytestmark
+@pytest.mark.skipif(os.getenv("TRAVIS", False) is True, reason="tests for localhost only")
 def test_not_found_404(client):
     """Start with a blank database."""
 
@@ -13,7 +12,8 @@ def test_not_found_404(client):
     assert rv.status_code == 404
     assert b'404 Not Found' in rv.data
 
-@pytestmark
+
+@pytest.mark.skipif(os.getenv("TRAVIS", False) is True, reason="tests for localhost only")
 def test_update_task(client):
     data = {
         'title': 'test_task_creation'
@@ -31,7 +31,8 @@ def test_update_task(client):
     assert rv.status_code == 200
     assert json.loads(rv.data)['title'] == data['title']
 
-@pytestmark
+
+@pytest.mark.skipif(os.getenv("TRAVIS", False) is True, reason="tests for localhost only")
 def test_delete_non_existent(client):
     data = {
         'id': 'not_existent'
@@ -39,7 +40,8 @@ def test_delete_non_existent(client):
     rv = client.delete('/api/v1/tasks/%s' % str(data['id']))
     assert rv.status_code == 404
 
-@pytestmark
+
+@pytest.mark.skipif(os.getenv("TRAVIS", False) is True, reason="tests for localhost only")
 def test_create_list_and_delete_task(client):
     data = {
         'title': 'test_task_creation'
